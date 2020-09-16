35b16b1df112c965cb0a30e1d32cf9c0c9841497
Kunal Kumar <kunalkseven@gmail.com>
2020-09-16 09:40:04 +0530

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <aboutus/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Aboutus from './components/About_us.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Aboutus
   }
 }
 </script>
---
+++ src/components/About_us.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="about">
+        <div class="container vision">
+            <div class="row">
+                <div class="col-md-6 vision-img">
+                    <img src="../assets/Asset-49.svg" alt="">
+                </div>
+                <div class="col-md-6 vesion-text">
+                    <h1 class="h1-text">Our Vision</h1>
+                    <p class="p-text">Everyday travel, solved! We are setting a trendier trend by transforming the daily travel ‘culture’ into something better. We call it Ontrack.</p>
+                </div>
+            </div>
+        </div>
+        <div class="mission-box2">
+                
+        </div>
+        <div class="mission-box1">
+        <div class="container mission">
+            <div class="row">
+                <div class="col-md-6 vesion-text">
+                    <h1 class="h1-mission">Our Mission</h1>
+                    <p class="p-mission">Travel goals: We want day-to-day travellers to ride with us on our monthly rented and utility bikes. Our aim is to make their travel experience uncomplicated and enjoyable.</p>
+                </div>
+                <div class="col-md-6 mission-img">
+                    <img src="../assets/about-mission1.svg" alt="">
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="container affordable">
+            <div class="row">
+                <div class="col-md-6 affordable-img">
+                    <img src="../assets/Asset-49.svg" alt="">
+                </div>
+                <div class="col-md-6 vesion-text">
+                    <h1 class="h1-text">Affordable & Independent</h1>
+                    <p class="p-text"><strong>Affordable and Independent:</strong> Making day-to-day travel pocket-friendly, hassle-free and effortless! Become more self-sufficient and free-spirited with Ontrack.</p> 
+                    <p class="p-text"><strong>Handpick your style:</strong> Select from the range of models that we offer. <br>
+                    Get one that gets your personality going. You have the choice to upgrade it to a different model every month!</p>
+                </div>
+            </div>
+        </div>
+        <div class="ref">
+                <div class="ref-img">
+                    <img class="ref-img" src="../assets/refer-footer1.svg" alt="">
+                </div>
+                <div class="ref-text">
+                    <h1 class="ref-h1">Refer a friend</h1>
+                    <p class="ref-p">Click Here To Invite Your Friend To Book A Bike And Earn 250 Ontrack Points.</p>
+                </div>
+                <div class="ref-btn">
+                    <button class="btn btn-primary" type="submit">Refer Now</button>
+                </div>
+        </div>
+    </div>
+</template>
+<script>
+import '../assets/style/style.css'
+export default {
+    name: "Aboutus"
+}
+</script>
